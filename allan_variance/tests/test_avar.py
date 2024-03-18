244aa9af277ff1b099e78b168ed09a09815d49ce
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2024-03-18 23:15:08 +0500

    MNT: Rename package from avar into allan_variance

--- allan_variance/tests/test_avar.py
+++ allan_variance/tests/test_avar.py
@@ -1,26 +1,26 @@
 import numpy as np
-import avar
+import allan_variance
 
 
 def test_generate_avar_smoke():
     x = np.ones(1000)
-    tau, av = avar.compute_avar(x)
-    assert np.all(av == 0.0)
+    tau, avar = allan_variance.compute_avar(x)
+    assert np.all(avar == 0.0)
 
     x = np.arange(1000)
-    tau, av = avar.compute_avar(x)
-    assert np.all(av == 0.5 * tau ** 2)
+    tau, avar = allan_variance.compute_avar(x)
+    assert np.all(avar == 0.5 * tau ** 2)
 
 
 def test_estimate_parameters_smoke():
     rng = np.random.RandomState(0)
     x = rng.randn(1000)
-    tau, av = avar.compute_avar(x)
-    parameters, _ = avar.estimate_parameters(tau, av)
+    tau, avar = allan_variance.compute_avar(x)
+    parameters, _ = allan_variance.estimate_parameters(tau, avar)
     assert np.abs(parameters.white - 1.0) < 0.1
 
     effects = ['quantization', 'white', 'flicker', 'walk', 'ramp']
     for exclude in ['quantization', 'flicker', 'walk', 'ramp']:
         effects.remove(exclude)
-        parameters, _ = avar.estimate_parameters(tau, av, effects)
+        parameters, _ = allan_variance.estimate_parameters(tau, avar, effects)
         assert np.abs(parameters.white - 1.0) < 0.1
